97fde5e928f93410dac5e9aa40c68fdf72c15467
freetimezp <kraftukrnet@ukr.net>
2023-08-20 19:01:32 +0300

    09. js-cars-05. Change bg video on slide.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,34 @@ function toggleMenu() {
     menu.classList.toggle('active');
     nav.classList.toggle('active');
 }
+
+//change bg video 
+function changeVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+        }
+    })
+
+    models.forEach(model => {
+        model.classList.remove('active');
+
+        if (model.classList.contains(name)) {
+            model.classList.add('active');
+        }
+    })
+
+    trailers.forEach(trailer => {
+        trailer.classList.remove('active');
+
+        if (trailer.classList.contains(name)) {
+            trailer.classList.add('active');
+        }
+    })
+}
